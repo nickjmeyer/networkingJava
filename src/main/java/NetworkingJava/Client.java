289a1c1960ed0b99d16ff6e55834d3112cfcc23a
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-11-15 19:21:16 -0500

    use new setup in server and client scripts

--- src/main/java/NetworkingJava/Client.java
+++ src/main/java/NetworkingJava/Client.java
@@ -4,32 +4,55 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-
     public static void main(String [] args) {
-        String serverName = "127.0.0.1";
-        int port;
-        if (args.length > 0)
-            port = Integer.parseInt(args[0]);
-        else
-            port = 7777;
+        Hive hive = new EchoHive();
         try {
-            System.out.println("Connecting to " + serverName
-                    + " on port " + port);
-            Socket client = new Socket(serverName, port);
+            InetAddress address = InetAddress.getByName("localhost");
+            Connection conn = Connection.connect_to(hive,address,7777);
 
-            System.out.println("Just connected to "
-                    + client.getRemoteSocketAddress());
-            OutputStream outToServer = client.getOutputStream();
-            DataOutputStream out = new DataOutputStream(outToServer);
+            int N = 5;
+            for (int i = 0; i < N; i++) {
+                String msg = "Message " + i;
+                hive.send(conn,msg.getBytes("UTF-8"));
+                Thread.sleep(500);
+            }
+            conn.close();
 
-            out.writeUTF("Hello from " + client.getLocalSocketAddress());
-            InputStream inFromServer = client.getInputStream();
-            DataInputStream in = new DataInputStream(inFromServer);
-
-            System.out.println("Server says " + in.readUTF());
-            client.close();
-        }catch(IOException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
+
+// public class Client {
+
+//     public static void main(String [] args) {
+//         String serverName = "127.0.0.1";
+//         int port;
+//         if (args.length > 0)
+//             port = Integer.parseInt(args[0]);
+//         else
+//             port = 7777;
+//         try {
+//             System.out.println("Connecting to " + serverName
+//                     + " on port " + port);
+//             Socket client = new Socket(serverName, port);
+
+//             System.out.println("Just connected to "
+//                     + client.getRemoteSocketAddress());
+//             OutputStream outToServer = client.getOutputStream();
+//             DataOutputStream out = new DataOutputStream(outToServer);
+
+//             out.writeUTF("Hello from " + client.getLocalSocketAddress());
+//             InputStream inFromServer = client.getInputStream();
+//             DataInputStream in = new DataInputStream(inFromServer);
+
+//             System.out.println("Server says " + in.readUTF());
+//             client.close();
+//         }catch(IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
--- src/main/java/NetworkingJava/Server.java
+++ src/main/java/NetworkingJava/Server.java
@@ -3,56 +3,83 @@ package NetworkingJava;
 import java.net.*;
 import java.io.*;
 
-public class Server extends Thread {
-    private ServerSocket serverSocket;
-
-    public Server(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
-        // serverSocket.setSoTimeout(10000);
-    }
-
-    public void run() {
-        while(true) {
-            try {
-                System.out.println("Waiting for client on port " +
-                        serverSocket.getLocalPort() + "...");
-                Socket server = serverSocket.accept();
-
-                System.out.println("Just connected to "
-                        + server.getRemoteSocketAddress());
-                DataInputStream in = new DataInputStream(
-                        server.getInputStream());
-
-                System.out.println(in.readUTF());
-                DataOutputStream out = new DataOutputStream(
-                        server.getOutputStream());
-                out.writeUTF("Thank you for connecting to "
-                        + server.getLocalSocketAddress()
-                        + "\nGoodbye!");
-                server.close();
-
-            }catch(SocketTimeoutException s) {
-                System.out.println("Socket timed out!");
-                break;
-            }catch(IOException e) {
-                e.printStackTrace();
-                break;
-            }
-        }
-    }
-
+public class Server {
     public static void main(String [] args) {
-        int port;
-        if (args.length > 0)
-            port = Integer.parseInt(args[0]);
-        else
-            port = 7777;
-
+        Hive hive = new EchoHive();
         try {
-            Thread t = new Server(port);
-            t.start();
-        }catch(IOException e) {
+            InetAddress address;
+            address = InetAddress.getByName("127.0.0.1");
+            Acceptor acceptor = new Acceptor(hive,7777,50,address);
+
+            acceptor.start();
+
+            int N = 20;
+            for (int i = 0; i < N; i++) {
+                Thread.sleep(1000);
+            }
+            acceptor.close();
+            acceptor.join();
+            hive.close();
+        } catch (UnknownHostException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            hive.close();
         }
     }
 }
+
+// public class Server extends Thread {
+//     private ServerSocket serverSocket;
+
+//     public Server(int port) throws IOException {
+//         serverSocket = new ServerSocket(port);
+//         // serverSocket.setSoTimeout(10000);
+//     }
+
+//     public void run() {
+//         while(true) {
+//             try {
+//                 System.out.println("Waiting for client on port " +
+//                         serverSocket.getLocalPort() + "...");
+//                 Socket server = serverSocket.accept();
+
+//                 System.out.println("Just connected to "
+//                         + server.getRemoteSocketAddress());
+//                 DataInputStream in = new DataInputStream(
+//                         server.getInputStream());
+
+//                 System.out.println(in.readUTF());
+//                 DataOutputStream out = new DataOutputStream(
+//                         server.getOutputStream());
+//                 out.writeUTF("Thank you for connecting to "
+//                         + server.getLocalSocketAddress()
+//                         + "\nGoodbye!");
+//                 server.close();
+
+//             }catch(SocketTimeoutException s) {
+//                 System.out.println("Socket timed out!");
+//                 break;
+//             }catch(IOException e) {
+//                 e.printStackTrace();
+//                 break;
+//             }
+//         }
+//     }
+
+//     public static void main(String [] args) {
+//         int port;
+//         if (args.length > 0)
+//             port = Integer.parseInt(args[0]);
+//         else
+//             port = 7777;
+
+//         try {
+//             Thread t = new Server(port);
+//             t.start();
+//         }catch(IOException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
